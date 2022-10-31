38776ed4a44c3a38fe46416193e960e84f4bc9ec
Zoe Guidry <oyezoey@gmail.com>
2022-10-31 10:04:57 -0500

    hey

--- util.js
+++ util.js
@@ -1,7 +1,7 @@
-function getCube (x, y, z) {
-    return new THREE.BoxGeometry(x,y,z);
-}
+// function getCube (x, y, z) {
+//     return new THREE.BoxGeometry(x,y,z);
+// }
 
-function (cube) {
-    cube = new
-}
+// function (cube) {
+//     cube = new
+// }
